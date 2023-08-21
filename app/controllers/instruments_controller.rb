2c9e2561e2b8e991a55b84f299c90f21cf0609f6
William Muhirwa <muhirwawilliam245@gmail.com>
2023-08-21 14:43:03 +0200

    some edits to show

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,10 @@
 class InstrumentsController < ApplicationController
+
+  def show
+    @instrument = Instrument.find(params[:id])
+    @name = @instrument.name
+    @city = @instrument.city
+  end
+
+
 end
