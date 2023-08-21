0fee18ba362549de3d0f2f1dd156509ea9f102d7
gianluca cattin <gcattin97@gmail.com>
2023-08-21 15:49:16 +0200

    search works

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,6 +2,13 @@ class InstrumentsController < ApplicationController
 
   def index
     @instruments = Instrument.all
+    if params[:city].present?
+      @instruments = @instruments.where(city: params[:city])
+    end
+
+    if params[:name].present?
+      @instruments = @instruments.where("name ILIKE ?", "%#{params[:name]}%")
+    end
   end
 
   def show
