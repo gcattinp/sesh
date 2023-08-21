234feeb18efd4b10c69edd40a61001afe0283046
gianluca cattin <gcattin97@gmail.com>
2023-08-21 14:50:49 +0200

    just some spacing issues

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,13 +1,13 @@
 class InstrumentsController < ApplicationController
-  
+
   def index
     @instrument = Instrument.all
   end
-  
+
   def show
     @instrument = Instrument.find(params[:id])
     @name = @instrument.name
     @city = @instrument.city
   end
-  
+
 end
